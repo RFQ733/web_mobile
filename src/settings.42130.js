84ca93bb9bebd97511f78fd2e694a1c0dbb3deb1
RFQ733 <2452185201@qq.com>
2021-01-21 00:57:58 +0800

    2

---
+++ src/settings.42130.js
@@ -0,0 +1,1 @@
+window._CCSettings={platform:"web-mobile",groupList:["zhangai","ren","parking"],collisionMatrix:[[false,true],[true,false,true],[false,true,false]],hasResourcesBundle:false,hasStartSceneBundle:false,remoteBundles:[],subpackages:[],launchScene:"db://assets/begin.fire",orientation:"",jsList:[],bundleVers:{internal:"279e0",main:"98d97"}};
